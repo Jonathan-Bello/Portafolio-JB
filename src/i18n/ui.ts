2f98d25d9380519ad3b3453b8e2d2b5deae8abd7
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2025-04-02 00:56:26 -0600

    add: configuracion inicial del cambio de traducción

---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+
+export const ui = {
+    es: {
+        "nav.home": "Inicio",
+        "nav.projects": "Proyectos",
+        "nav.blog": "Blog",
+        "nav.about": "Acerca de",
+        "nav.contact": "Contacto",
+    },
+    en: {
+        "nav.home": "Home",
+        "nav.projects": "Projects",
+        "nav.blog": "Blog",
+        "nav.about": "About",
+        "nav.contact": "Contact",
+    },
+}
