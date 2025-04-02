702216ccab1344cec19e8933847ba0c7ddc4e072
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2025-04-02 03:45:25 -0600

    add: funcionalidad del cambio de idioma

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,20 +5,21 @@ export const languages = {
 
 export const defaultLang = "es";
 
+export const showDefaultLang = false;
 
 export const ui = {
-    es: {
-        "nav.home": "Inicio",
-        "nav.projects": "Proyectos",
-        "nav.blog": "Blog",
-        "nav.about": "Acerca de",
-        "nav.contact": "Contacto",
-    },
-    en: {
-        "nav.home": "Home",
-        "nav.projects": "Projects",
-        "nav.blog": "Blog",
-        "nav.about": "About",
-        "nav.contact": "Contact",
-    },
-}
+  es: {
+    "nav.home": "Inicio",
+    "nav.projects": "Proyectos",
+    "nav.blog": "Blog",
+    "nav.about": "Acerca de",
+    "nav.contact": "Contacto",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.projects": "Projects",
+    "nav.blog": "Blog",
+    "nav.about": "About",
+    "nav.contact": "Contact",
+  },
+};
